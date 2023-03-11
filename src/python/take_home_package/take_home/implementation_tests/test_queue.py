4593c5635b6903bdda65a9521f4f67a814310560
ishank09 <ishankvasania09@gmail.com>
2023-03-11 11:18:16 -0800

    update directory

--- src/python/take_home_package/take_home/implementation_tests/test_queue.py
+++
@@ -1,9 +0,0 @@
-''' Copyright (c) Docugami, Inc. All rights reserved. '''
-
-from take_home.implementation.queue import Queue
-
-def test_peek():
-    ''' Simple test of the peek method. '''
-    queue = Queue(10)
-    queue.enqueue(1)
-    assert queue.peek() == 1
